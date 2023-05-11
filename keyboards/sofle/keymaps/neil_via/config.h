69f27e1b0ac46f628ed924fe9e1310ecdd4e8c4e
neilhump <neilhump@gmail.com>
2023-05-11 16:06:52 +0200

    1

--- keyboards/sofle/keymaps/neil_via/config.h
+++ keyboards/sofle/keymaps/neil_via/config.h
@@ -21,6 +21,7 @@
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
 
 #if defined(KEYBOARD_sofle_rev1)
 // Add RGB underglow and top facing lighting
